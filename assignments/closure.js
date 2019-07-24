3255375261ed292c6746dd0b457d90f62aaa00fb
Angela Flowers <me@angela.flowers>
2019-07-23 22:06:37 -0400

    closure.js stretch problems

--- assignments/closure.js
+++ assignments/closure.js
@@ -18,14 +18,58 @@ console.log(myFunction());
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let num = 0;
+  return function countingFunct () {
+    num++;
+    return num;
+  }
 };
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
 
+const newCounter = counter();
+console.log(counter());
+
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
+
+
+const thirdCounter = counter();
+console.log(thirdCounter());
+
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
-const counterFactory = () => {
+const counterFactory = (initValue) => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let count = initValue 
+  return {
+      "increment": function() {
+      count++;
+      return count;
+    },
+
+    "decrement": function() {
+      count--;
+      return count;
+    }
+  }
 };
+
+const countDown = counterFactory(100).decrement;
+console.log(countDown());
+console.log(countDown());
+console.log(countDown());
+console.log(countDown());
+console.log(countDown());
+console.log(countDown());
+console.log(countDown());
+
+const countUp = counterFactory(10).increment;
+console.log(countUp());
+console.log(countUp());
+console.log(countUp());
+console.log(countUp());
